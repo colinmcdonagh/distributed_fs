7982066831de32203500b585372e4acfb1cbb1c1
Colin McDonagh <24464477+ctmcdo@users.noreply.github.com>
2017-12-03 21:45:31 +0000

    sort out architectural plan

--- README.md
+++ README.md
@@ -5,3 +5,9 @@ commands for transparently accessing distributed file system:
 	* ls
 	* cat %file
 	* mv %local_file %remote_file
+
+TODO:
+
+1. modify controller to only return server addresses
+2. modify proxy to first make request to controller, and then directly to file servers
+3. modify file server to actually serve up shtuff.
--- proxy/proxy.go
+++ proxy/proxy.go
@@ -1,5 +1,40 @@
-package main
+package proxy
 
-func main() {
+import (
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"os"
+)
+
+type proxy struct {
+	controllerAddr string
+}
+
+func (p *proxy) fileAddr(file string) string {
+	return fmt.Sprintf("%s/%s", p.controllerAddr, file)
+}
+
+func (p *proxy) download(file string) *os.File {
+	resp, err := http.Get(fmt.Sprintf(p.fileAddr(file)))
+	if err != nil {
+		// handle error
+	}
+	defer resp.Body.Close()
+	fileBytes, _ := ioutil.ReadAll(resp.Body)
+	filePtr, err := os.Create(file)
+	if err != nil {
+		// handle error
+	}
+	filePtr.Write(fileBytes)
+	return filePtr
+}
+
+func (p *proxy) upload(localfile, remotefile string) {
+
+	var r io.Reader
+	r, _ = os.Open(localfile)
+	resp, err := http.Post(p.fileAddr(remotefile), "text/plain", r)
 
 }
